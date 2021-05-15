a7e6902b65a5d712d2030fc1fd6662bc5671906f
MarcusLucinda <marcusvbl@hotmail.com>
2021-05-15 17:06:34 -0300

    added consumers

---
+++ src/main/java/com/lucinda/EmailService.java
@@ -0,0 +1,50 @@
+package com.lucinda;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class EmailService {
+
+	public static void main(String[] args) {
+		var consumer = new KafkaConsumer<String, String>(properties());
+		consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
+		while(true) {
+			var records = consumer.poll(Duration.ofMillis(100));
+			if(!records.isEmpty()){
+				System.out.println("Found" + records.count() + "registers");
+				for(var record : records) {
+					System.out.println("-----------------------------------");
+					System.out.println("Sending email");
+					System.out.println(record.key());
+					System.out.println(record.value());
+					System.out.println(record.partition());
+					System.out.println(record.offset());
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("Order processed");
+				}
+			}
+		}
+		
+	}
+	
+	public static Properties properties() {
+		var properties = new Properties();
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+		return properties;
+	}
+	
+	
+	
+}
--- src/main/java/com/lucinda/NewOrderMain.java
+++ src/main/java/com/lucinda/NewOrderMain.java
@@ -1,5 +1,6 @@
 package com.lucinda;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,13 +15,17 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "132123,67523,7894589745";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if(ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset "  + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+            System.out.println("Success enviando " + data.topic() + ":::partition " + data.partition() + "/ offset "  + data.offset() + "/ timestamp " + data.timestamp());
+        };
+		producer.send(record, callback).get();
+		var email = "We're processing your order";
+		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
