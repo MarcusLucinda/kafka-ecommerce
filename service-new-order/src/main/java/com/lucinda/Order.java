d2f6ba73c1ee4889f5731961bdd90d5b855cd904
MarcusLucinda <marcusvbl@hotmail.com>
2021-05-18 17:30:57 -0300

    modules structure

---
+++ service-new-order/src/main/java/com/lucinda/Order.java
@@ -0,0 +1,19 @@
+package com.lucinda;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+	private final String userId, orderId;
+	private final BigDecimal amount;
+	
+	public Order(String userId, String orderId, BigDecimal amount) {
+		super();
+		this.userId = userId;
+		this.orderId = orderId;
+		this.amount = amount;
+	}
+	
+	
+	
+}
